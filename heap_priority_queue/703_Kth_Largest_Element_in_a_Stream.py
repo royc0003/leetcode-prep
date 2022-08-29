33043a51eb35595470e9657f02df627bee5ff5df
Royce <royceang96@gmail.com>
2022-08-29 20:12:35 +0800

    included Time Complexity

--- heap_priority_queue/703_Kth_Largest_Element_in_a_Stream.py
+++ heap_priority_queue/703_Kth_Largest_Element_in_a_Stream.py
@@ -5,22 +5,25 @@ class KthLargest:
     [1,2,3,4]
     however, essentially, we can always maintain the array such that the length of the array == k.
     and return the final element. e.g. [1,2,3]
+    overall time complexity:
+    constuctor + m-times of `add`:
+    O(lgN + M*lgN)
     '''
 
-    def __init__(self, k: int, nums: List[int]):
+    def __init__(self, k: int, nums: List[int]):# Constructor's time complexity: O(N + N*lgN) => O(lgN)
         self.minHeap, self.k = nums, k 
-        heapq.heapify(self.minHeap)
-        while len(self.minHeap) > self.k:
-            heapq.heappop(self.minHeap)
+        heapq.heapify(self.minHeap) # O(N)
+        while len(self.minHeap) > self.k: # O(N-k * lgN); and if k == 1, we can reduce to O(N*lgN)
+            heapq.heappop(self.minHeap) 
         
 
-    def add(self, val: int) -> int:
-        heapq.heappush(self.minHeap, val)
-        if len(self.minHeap) > self.k:
+    def add(self, val: int) -> int:       # M times of add(); O(M*lgN)
+        heapq.heappush(self.minHeap, val) # O(lgN)
+        if len(self.minHeap) > self.k:    # O(lgN)
             heapq.heappop(self.minHeap)
         return self.minHeap[0] # return the smallest element, which is also essentially, the 3rd element.
         
-
+    
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
