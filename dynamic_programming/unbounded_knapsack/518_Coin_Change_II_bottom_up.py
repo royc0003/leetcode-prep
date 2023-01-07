23a9d635bab917707a69436c0f5810e61a6ee346
Royce <royceang96@gmail.com>
2023-01-07 13:15:26 -0800

    bottom up soln

---
+++ dynamic_programming/unbounded_knapsack/518_Coin_Change_II_bottom_up.py
@@ -0,0 +1,21 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+
+        # Create the DP
+        dp = [[0 for _ in range(amount+1)] for _ in range(len(coins))]
+        ROW, COL = len(dp), len(dp[0])
+        # fill the column on the most left with `1`
+        for i in range(ROW):
+            dp[i][0] = 1
+        
+        # true dp occurs here
+        for i in range(ROW):
+            for total in range(1, COL):
+                # skip
+                if i > 0:
+                    dp[i][total] = dp[i-1][total]
+                # don't skip
+                if total >= coins[i]:
+                    dp[i][total] += dp[i][total-coins[i]]
+
+        return dp[ROW-1][COL-1] if dp[ROW-1][COL-1] != float('inf') else 0 
