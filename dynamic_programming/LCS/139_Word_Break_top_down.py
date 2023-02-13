6c68149c1cf952735af0cfad0810c32ebcedcf30
Royce <royceang96@gmail.com>
2023-02-13 15:52:17 -0800

    top_down soln

---
+++ dynamic_programming/LCS/139_Word_Break_top_down.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        i, j = 0, 0 # i = s pointer, j = dictionary pointer
+        cache = {}
+        def check_word(i):
+            if i >= len(s):
+                return True
+            if (i) in cache:
+                return cache[i]
+
+            accept = False
+            cache[i] = accept
+            for j, dict_word in enumerate(wordDict):
+                if (i+len(dict_word)-1) < len(s) and s[i: i+len(dict_word)] == dict_word:
+                    cache[i] = check_word(i+len(wordDict[j]))
+                    # accept = check_word(i+len(wordDict[j]), j)
+                    if cache[i]:
+                        break
+            return cache[i]
+        return check_word(0)
